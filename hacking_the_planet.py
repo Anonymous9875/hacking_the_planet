82415f8d27aa6ba8268883e7e4e60f3f21e08997
AnonNews_irc <144509894+Anonymous9875@users.noreply.github.com>
2023-12-05 19:23:54 -0500

    Add files via upload

--- hacking_the_planet.py
+++ hacking_the_planet.py
@@ -1,6 +1,6 @@
 import requests, re
 from colorama import Fore, Back, Style
-print(Fore.YELLOW +"""
+print("""
  db   db  .d8b.   .o88b. db   dD d888888b d8b   db  d888b    d888888b db   db d88888b 
  88   88 d8' `8b d8P  Y8 88 ,8P'   `88'   888o  88 88' Y8b   `~~88~~' 88   88 88'     
  88ooo88 88ooo88 8P      88,8P      88    88V8o 88 88           88    88ooo88 88ooooo 
@@ -48,10 +48,10 @@ o0ooo°oo+++-,.               ,°    ,--,=+..
 (FOR MORE PRIVACY): Copy the IP open Them in The TOR Browser :)
 """)
 print("""\033[1;37m
-==============================================================================================================================================
+====================================================================================================================================================================
  \033[1;35m[#] \033[1;37mCoded By : AnonNews_irc 
  \033[1;35m[#] \033[1;37mTelegram : https://t.me/addlist/iZfJw-LVfYthNzYx                                                                                   
-==============================================================================================================================================
+============================================================================================================================================================================================
  \033[1;35m[1] \033[1;37mUnited States                              \033[1;35m[33] \033[1;37mFinland                                    \033[1;35m[65] \033[1;37mNicaragua
  \033[1;35m[2] \033[1;37mJapan                                      \033[1;35m[34] \033[1;37mGreece                                     \033[1;35m[66] \033[1;37mSingapore
  \033[1;35m[3] \033[1;37mTaiwan, Province Of                        \033[1;35m[35] \033[1;37mAustralia                                  \033[1;35m[67] \033[1;37mSaudi Arabia
@@ -67,7 +67,7 @@ print("""\033[1;37m
  \033[1;35m[13] \033[1;37mCanada                                    \033[1;35m[45] \033[1;37mEstonia                                    \033[1;35m[77] \033[1;37mSaint Kitts And Nevis
  \033[1;35m[14] \033[1;37mPoland                                    \033[1;35m[46] \033[1;37mEcuador                                    \033[1;35m[78] \033[1;37mCayman Islands
  \033[1;35m[15] \033[1;37mNorway                                    \033[1;35m[47] \033[1;37mColombia                                   \033[1;35m[79] \033[1;37mHonduras
- \033[1;35m[16] \033[1;37mNetherlands                               \033[1;35m[48] \033[1;37mBosnia And Herzegovina                     \033[1;35m[80] \033[1;37mGeorgiam
+ \033[1;35m[16] \033[1;37mNetherlands                               \033[1;35m[48] \033[1;37mBosnia And Herzegovina                     \033[1;35m[80] \033[1;37Georgiam
  \033[1;35m[17] \033[1;37mRomania                                   \033[1;35m[49] \033[1;37mIceland                                    \033[1;35m[81] \033[1;37mAlgeria
  \033[1;35m[18] \033[1;37mSweden                                    \033[1;35m[50] \033[1;37mSlovenia                                   \033[1;35m[82] \033[1;37mNew Caledonia
  \033[1;35m[19] \033[1;37mSpain                                     \033[1;35m[51] \033[1;37mMalaysia                                   \033[1;35m[83] \033[1;37mMacao
@@ -83,14 +83,14 @@ print("""\033[1;37m
  \033[1;35m[29] \033[1;37mIreland                                   \033[1;35m[61] \033[1;37mArmenia                                    \033[1;35m[93] \033[1;37mMacedonia
  \033[1;35m[30] \033[1;37mIndia                                     \033[1;35m[62] \033[1;37mPortugal                                   \033[1;35m[94] \033[1;37mGuernsey
  \033[1;35m[31] \033[1;37mIndonesia                                 \033[1;35m[63] \033[1;37mCroatia                                    \033[1;35m[95] \033[1;37mParaguay
- \033[1;35m[32] \033[1;37mSouth Africa                              \033[1;35m[64] \033[1;37mTunisia                                    
-==============================================================================================================================================
+ \033[1;35m[32] \033[1;37mSouth Africa                              \033[1;35m[64] \033[1;37mTunisia
+============================================================================================================================================================================================
 
 """)
 
 try:
     print()
-    countries = ["US", "JP", "TW", "IT", "RU", "KR", "DE", "FR", "CZ", "GB", "NL", "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "PL", "IR","NO", "RO", "IN", "VN", "BE", "BR", "BG", "ID", "DK", "AR", "MX", "FI", "CN", "CL", "ZA", "SK", "HU", "IE", "EG", "TH", "UA", "RS", "HK", "GR", "PT", "LV", "SG", "IS", "MY", "CO", "TN", "EE", "DO", "SI", "EC", "LT", "PS", "NZ", "BD", "PA", "MD", "NI", "MT", "IT", "SA", "HR", "CY", "PK", "AE", "KZ", "KW", "VE", "GE", "ME", "SV", "LU", "CW", "PR", "CR", "BY", "AL", "LI", "BA", "PY", "PH", "FO", "GT", "NP", "PE", "UY","-"]
+    countries = ["US", "JP", "TW", "IT", "RU", "KR", "DE", "FR", "CZ", "AT", "CH", "GB", "CA", "PL", "NO", "NL", "RO", "SE", "ES", "VN", "BG", "DK", "IR", "BE", "BR", "TR", "RS", "SK", "IE", "IN", "ID", "ZA", "FI", "GR", "AU", "HU", "LV", "AR", "UA", "TH", "IL", "MX", "LT", "NZ", "EE", "EC", "CO", "BA", "IS", "SI", "MY", "CN", "MD", "CL", "BD", "HK", "SY", "KZ", "FO", "ME", "AM", "PT", "HR", "TN", "NI", "SG", "SA", "PA", "NG", "LU", "PE", "BY", "TT", "AO", "AW", "PR", "KN", "KY", "HN", "GE", "DZ", "NC", "MO", "BB", "GP", "GU", "AD", "CR", "LA", "TZ", "MT", "MG", "MK", "GG", "PY","-"]
     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
 
 # google dorks
@@ -109,7 +109,7 @@ try:
     num = int(input("Enter a number ---> "))
     print("""
     \033[1;35m
-    [ Searching the IP of the cameras... ]
+    [ Scraping IP Hosted Cameras... ]
     
     """)
     if num not in range(1, 21+1):
